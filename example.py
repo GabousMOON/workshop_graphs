4d5263f2440d36d7488594de569ab4967f0559af
Gabe <soccernerdgj@gmail.com>
2023-05-30 14:00:26 -0600

    first graph

--- example.py
+++ example.py
@@ -1,4 +1,14 @@
-import bokeh as bk
+from bokeh.plotting import figure, show
 import numpy as np
 import sympy as sp
 
+x_vals = np.arange(-10, 10)
+y_vals = x_vals
+
+fig = figure(
+    title='Initial Graph', x_axis_label = "x axis", y_axis_label = 'y axis'
+)
+
+fig.line(x_vals, y_vals, legend_label = "Temp.", line_width=2)
+
+show(fig) # type: ignore
