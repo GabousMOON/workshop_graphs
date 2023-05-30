e4ca2ecffba5f3a2d15db2c2c870e397f0f4f945
Gabe <soccernerdgj@gmail.com>
2023-05-30 14:35:12 -0600

    Initial styling with responsive plot and axis

--- example.py
+++ example.py
@@ -1,14 +1,44 @@
 from bokeh.plotting import figure, show
+from bokeh.io import curdoc
 import numpy as np
 import sympy as sp
 
+'''
+    Built in themes to choose from are
+    "caliber", "dark_minimal", "light_minimal", "night_sky", "contrast"
+'''
+
+curdoc().theme = "dark_minimal"
 x_vals = np.arange(-10, 10)
 y_vals = x_vals
 
+'''
+    #1. Setting the width and height
+    #2. Responsive plot sizing
+    #3. Setting axes' appearance
+    #4. Axis Ranges
+    #5. Positioning the axis
+
+'''
+
+
 fig = figure(
-    title='Initial Graph', x_axis_label = "x axis", y_axis_label = 'y axis'
+    title="Initial Graph",
+    width=350,                      #1
+    height=250,                     #1
+    sizing_mode = 'stretch_width',  #2
+    y_range=(-10, -10),             #4
+
 )
 
-fig.line(x_vals, y_vals, legend_label = "Temp.", line_width=2)
+fig.xaxis.axis_label = "X values"   #3
+fig.xaxis.axis_line_width = 1       #3
+fig.xaxis.axis_line_color = "red"   #3
 
-show(fig) # type: ignore
+fig.axis[0].fixed_location = 0      #5
+fig.axis[1].fixed_location = 0      #5
+
+
+fig.line(x_vals, y_vals, legend_label="Temp.", line_width=2)
+
+show(fig)  # type: ignore
